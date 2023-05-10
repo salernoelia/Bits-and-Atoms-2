13891f99e137885f7f22fdde95e414aa0db0ea56
salernoelia <putopet@gmail.com>
2023-05-10 10:06:19 +0200

    ju

--- First HTML/script.js
+++ First HTML/script.js
@@ -1,8 +1,8 @@
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-    circle(mouseX,mouseY,50)
-  }
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  circle(mouseX, mouseY, 50)
+}
