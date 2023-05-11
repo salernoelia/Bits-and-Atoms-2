9b8db4dcc24e91956c2c2a0c42d1196b96a6b163
salernoelia <putopet@gmail.com>
2023-05-11 16:48:14 +0200

    main mage

--- script.js
+++ script.js
@@ -1,42 +1,59 @@
 var canvas;
+let a = 0 
+let diameter = 40;
+let d = 300
+let amount = 50
+
+function preload() {
+  grain = loadImage("/grain.png");
+}
 
 function setup() {
+   
     let cnv = createCanvas(windowWidth, windowHeight);
     cnv.position(-9, -9);
+    cnv.elt.style.position = "fixed"
+
+
 
   }
   
   function draw() {
-    background(0);
-    translate(width/2, height/2)
-    ellipseMode(CENTER);
-    noStroke()
-    fill(50)
-  
-    for (let i = 0; i < 200; i+= 2) {
-      rotate(sin(frameCount/50))
-      circ(0+i%4,0+i*frameCount/30,i/6);
-      circ(0+-i%20,0+-i*frameCount/10,-i/20);
-    }
     
-    if (frameCount > 300) {
-      frameCount = -frameCount
+    background(68,13,15)
+
+    
+    translate(windowWidth/2,windowHeight/2)
+    //line
+    
+    for (let i = 0; i < width; i+= 3) {
+      
+      noStroke();
+      scale(0.999)
+      rotate(20) //simple rotation
+      noFill();
+      stroke(132,89,107);
+      a
+      // let offset = i*mouseY/6000;
+      let y = sinMovment(sin(frameCount/50), -600,200, 0) 
+      push();
+     
+      strokeWeight(0.5);
+      circle(y,i,d)
+      pop();    
+      
     }
+  
+    // increment angle
+    const increment = PI*2/50
+    a += increment
   }
   
-  function circ(x,y,dia) {
-    // ellipse(x,y,dia/x,dia/y)
-    // ellipse(-x,-y,dia/x,dia/y)
-    // ellipse(x*sin(frameCount/60),y*sin(frameCount/60),dia,dia)
-    // ellipse(-x*sin(frameCount/60),-y*sin(frameCount/60),dia,dia)
-    text("hi",x,y)
-    text("hi",-x,-y)
-    
-    // push()
-    // rotate(80)
-    // ellipse(x,y,dia,dia)
-    // ellipse(-x,-y,dia,dia)
-    // pop()
+  //sinus function
+  function sinMovment(angle, offset, minVal, maxVal) {
+    let factor = sin(angle + offset*5)
+    let sinMovementVal = map(factor, -1, -0.5, minVal, maxVal)
+    return sinMovementVal;
   }
   
   
