91bc8c70023140a99c784e4a8a44a7e0d2d6ccd6
salernoelia <putopet@gmail.com>
2023-05-17 11:40:39 +0200

    more

--- 8-js-beginning/script.js
+++ 8-js-beginning/script.js
@@ -1,3 +1,40 @@
 let message = "hello";
+let number = 424;
+let number2 = 452;
+let steppi = "soviet be there"
+const array1 = [50,10,24,53,243]
+const myParagraph = document.getElementById("calc")
+var wasItRight = document.getElementById("wasItRight");
+realAnswer = "ege";
 
-alert(message);
+const recipe = {
+    duration: "30min",
+    fko: "40min"
+}
+
+
+function clicked() {
+    if (document.getElementById("input").value == realAnswer) {
+    console.log("worked!")
+    wasItRight = "right";
+    } else {
+    wasItRight = "wrong";
+}
+
+
+
+if (number < number2) {
+    console.log("its true!")
+    myParagraph.innerHTML = number2;
+    console.log(myParagraph)
+
+}
+}
+
+
+
+// for (let i = 1; i < 100; i++){
+//     console.log(i%5)
+// }
+
+
